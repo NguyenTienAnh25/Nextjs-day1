c407a2f808a176d6bcd6aa885dbce9d58af6729c
Nguyễn Tiến Anh <anhtien2k00@gmail.com>
2021-12-01 22:31:27 +0700

    new nextjs

---
+++ new-nextjs/pages/api/index.tsx
@@ -0,0 +1,20 @@
+var Mock = require('mockjs')
+var Random = Mock.Random
+var data = Mock.mock({
+    'list|1-10': [{
+        'id|+1': 1,
+        "name": Random.name(),
+        "email" : Random.email(),
+        "address": Random.city(),
+        "color": Random.color()
+        
+    }]
+})
+
+export default function handler(req:any, res:any) {
+    if (req.method === 'GET') {
+        res.status(200).json(data.list)
+    } else {
+      
+    }
+  }
